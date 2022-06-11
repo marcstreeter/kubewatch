1cc55f466cd7e1517507575ce29490223182fca3
Jane Streeter <jane.america.streeter@gmail.com>
2022-06-11 12:14:21 -0500

    just checking that javascript still works

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,12 @@
+<script>
+  let count = 0;
+  const click = () => count++;
+</script>
+
 <h1>Welcome to KubeWatch</h1>
 <p>
   This is just the beginning of a website built on
   <a href="https://kit.svelte.dev">svelte-kit</a>
   and deployed with github actions!
 </p>
+<button on:click={click}>Clicked {count} times</button>
