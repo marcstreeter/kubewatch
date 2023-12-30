65b71fc81374c5c4565d3c89c323788f3a059791
ruth <ruth.ines.streeter@gmail.com>
2023-12-30 14:21:20 -0600

    testing ruths sert up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
   const click = () => count++;
 </script>
 
-<h1>Hi Ruthy!</h1>
+<h1>Hi Dad!</h1>
 <p>
   This is just the beginning of a website built on
   <a href="https://kit.svelte.dev">svelte-kit</a>
